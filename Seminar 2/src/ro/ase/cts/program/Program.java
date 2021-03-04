763ab68e24c1c38ee3304451cc0f8662b46c394a
Ioana B <buzaulioana@gmail.com>
2021-03-04 10:22:39 +0200

    readers

--- Seminar 2/src/ro/ase/cts/program/Program.java
+++ Seminar 2/src/ro/ase/cts/program/Program.java
@@ -4,14 +4,14 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.classes.Angajat;
-import ro.ase.cts.classes.Utils;
+import ro.ase.cts.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
