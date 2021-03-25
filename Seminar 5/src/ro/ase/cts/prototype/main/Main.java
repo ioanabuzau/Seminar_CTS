218c5ea3a37438d0157a272b3891574c8b8f52a8
Ioana B <buzaulioana@gmail.com>
2021-03-25 11:19:22 +0200

    Prototype

---
+++ Seminar 5/src/ro/ase/cts/prototype/main/Main.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.prototype.main;
+
+import ro.ase.cts.prototype.Reteta;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Reteta reteta1 = new Reteta("Apa oxigenata", 50);
+		Reteta reteta2 = (Reteta) reteta1.copiaza();
+		
+		System.out.println(reteta1);
+		System.out.println(reteta2);
+
+	}
+
+}
