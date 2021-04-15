6207606876742476695f95402ac1cde7e6ec1a66
Ioana B <buzaulioana@gmail.com>
2021-04-15 10:36:05 +0300

    Composite

---
+++ Seminar 8/src/ro/ase/cts/composite/clase/Item.java
@@ -0,0 +1,34 @@
+package ro.ase.cts.composite.clase;
+
+public class Item implements ComponentaAbstracta {
+ 
+	private String nume;
+
+	public Item(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+	@Override
+	public void afiseazaDescriere() {
+		System.out.println("Item: " + nume);
+	}
+
+	@Override
+	public void addComponenta(ComponentaAbstracta componentaAbstracta) throws Exception {
+		throw new Exception();
+		
+	}
+
+	@Override
+	public void stergeComponenta(ComponentaAbstracta componentaAbstracta) {
+		throw new IllegalArgumentException();
+	}
+
+	@Override
+	public ComponentaAbstracta getComponenta(int pozitie) {
+		throw new IllegalArgumentException();
+	}
+	
+	
+}
---
+++ Seminar 8/src/ro/ase/cts/facade/clase/GestionatMese.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.facade.clase;
+
+public class GestionatMese {
+
+}
